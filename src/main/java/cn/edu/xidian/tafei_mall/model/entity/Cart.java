4039c0da464443e9e20f303930aa4438c1bd3a1d
nuuuuuun <17353515271@163.com>
2025-03-28 21:41:24 +0800

    Update Cart.java

--- src/main/java/cn/edu/xidian/tafei_mall/model/entity/Cart.java
+++ src/main/java/cn/edu/xidian/tafei_mall/model/entity/Cart.java
@@ -3,10 +3,12 @@ package cn.edu.xidian.tafei_mall.model.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -30,7 +32,7 @@ public class Cart implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("购物车ID")
-    @TableId("cart_id")
+    @TableId(value = "cart_id", type = IdType.INPUT)
     private String cartId;
 
     @ApiModelProperty("用户ID")
@@ -44,4 +46,10 @@ public class Cart implements Serializable {
     @ApiModelProperty("更新时间")
     @TableField("updated_at")
     private LocalDateTime updatedAt;
+
+    public Cart() {
+        this.cartId = UUID.randomUUID().toString(); // 生成 UUID
+        this.createdAt = LocalDateTime.now(); // 设置创建时间
+        this.updatedAt = LocalDateTime.now(); // 设置更新时间
+    }
 }

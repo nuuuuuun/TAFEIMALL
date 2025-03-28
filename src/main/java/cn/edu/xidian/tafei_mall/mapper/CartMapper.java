d1e7c1b484d532cc9a1c5de670d0760a57444a37
nuuuuuun <17353515271@163.com>
2025-03-28 22:37:59 +0800

    Update CartMapper.java

--- src/main/java/cn/edu/xidian/tafei_mall/mapper/CartMapper.java
+++ src/main/java/cn/edu/xidian/tafei_mall/mapper/CartMapper.java
@@ -3,6 +3,8 @@ package cn.edu.xidian.tafei_mall.mapper;
 import cn.edu.xidian.tafei_mall.model.entity.Cart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 
 /**
  * <p>
@@ -14,5 +16,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface CartMapper extends BaseMapper<Cart> {
-
+  @Insert("INSERT INTO cart (cart_id, user_id, created_at, updated_at) " +
+            "VALUES (#{cartId}, #{userId}, #{createdAt}, #{updatedAt})")
+    @Options(useGeneratedKeys = true, keyProperty = "cartId")
+    int insertCart(Cart cart);
 }
